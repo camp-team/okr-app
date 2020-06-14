6016ef98747b2cbe9fc2e457e3e32289eb1fb108
peyo-code <mr233425@gmail.com>
2020-06-14 14:10:09 +0900

    feat(home):homeモジュールの追加

--- src/app/manage/manage-routing.module.ts
+++ src/app/manage/manage-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { EditComponent } from '../edit/edit/edit.component';
+import { HomeComponent } from '../home/home/home.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'edit',
         component: EditComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
       },
     ],
   },
