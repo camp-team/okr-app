3a4ccb176cb2fa36879b673eef0e0fa0fe2db010
peyo-code <mr233425@gmail.com>
2020-06-17 20:31:08 +0900

    feat(firebase):add firebaseとAngularの連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDn9iFnVkalY34LDtOZN8EIfESlYVDihtA',
+    authDomain: 'okr-app-de4fa.firebaseapp.com',
+    databaseURL: 'https://okr-app-de4fa.firebaseio.com',
+    projectId: 'okr-app-de4fa',
+    storageBucket: 'okr-app-de4fa.appspot.com',
+    messagingSenderId: '104962990037',
+    appId: '1:104962990037:web:a6c1523b55ee08e88ea75c',
+    measurementId: 'G-YHBESMER0K',
+  },
 };
 
 /*
