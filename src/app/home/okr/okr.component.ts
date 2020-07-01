791fc82bab473fcf74e6981fbd7f2c637618311b
peyo-code <mr233425@gmail.com>
2020-07-01 23:39:12 +0900

    feat(okr): home componentの中に okr component生成

---
+++ src/app/home/okr/okr.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-okr',
+  templateUrl: './okr.component.html',
+  styleUrls: ['./okr.component.scss'],
+})
+export class OkrComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
