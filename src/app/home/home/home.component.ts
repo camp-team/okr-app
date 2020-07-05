976db55e13203e1aa2d58fa1bc3a9f670c3b0de4
peyo-code <mr233425@gmail.com>
2020-07-05 19:28:25 +0900

    feat(okr): home.component.tsでokrインターフェースの定義

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Okr } from 'src/app/interfaces/okr';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  okr: Okr = {
+    title: 'タイトル:　',
+    duration: '期間:　',
+    primary1: '',
+    primary2: '',
+    primary3: '',
+    trainerId: '',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
