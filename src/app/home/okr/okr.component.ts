82313afd63a9764f1605eeb81f1dcec6f4db000a
peyo-code <mr233425@gmail.com>
2020-07-05 19:11:20 +0900

    feat(okr): okrの型を含むオブジェクトの取得

--- src/app/home/okr/okr.component.ts
+++ src/app/home/okr/okr.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Okr } from 'src/app/interfaces/okr';
 
 @Component({
   selector: 'app-okr',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./okr.component.scss'],
 })
 export class OkrComponent implements OnInit {
+  @Input() okr: Okr;
+
   constructor() {}
 
   ngOnInit(): void {}
