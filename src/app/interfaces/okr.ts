99076609d5b593cd2d6d438729d675d636b39889
peyo-code <mr233425@gmail.com>
2020-07-09 21:14:03 +0900

    feat(okr): Firestoreと日付の互換性を持たせる

--- src/app/interfaces/okr.ts
+++ src/app/interfaces/okr.ts
@@ -1,6 +1,8 @@
+import { firestore } from 'firebase';
+
 export interface Okr {
   title: string;
-  duration: string;
+  duration: firestore.Timestamp;
   primary1: string;
   primary2: string;
   primary3: string;
