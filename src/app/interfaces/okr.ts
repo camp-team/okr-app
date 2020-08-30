b0a10cc948d27d46745eaaec4a2b2eddccd8a3b2
peyo-code <mr233425@gmail.com>
2020-08-30 16:56:37 +0900

    fix(edit.component):primary1,2,3をprimariesに変更

--- src/app/interfaces/okr.ts
+++ src/app/interfaces/okr.ts
@@ -1,12 +1,9 @@
 import { firestore } from 'firebase';
-
 export interface Okr {
   title: string;
   start: firestore.Timestamp;
   end: firestore.Timestamp;
-  primary1: string;
-  primary2: string;
-  primary3: string;
+  primaries: string;
   trainerId: string;
   id: string;
 }
