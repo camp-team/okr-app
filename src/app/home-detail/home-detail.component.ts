a5d5b01d932b0732665b44b68032de1a7f33fa99
peyo-code <mr233425@gmail.com>
2020-09-03 02:06:10 +0900

    feat(homedetail): 編集フォームで初期値をセット

--- src/app/home-detail/home-detail.component.ts
+++ src/app/home-detail/home-detail.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Okr } from '../interfaces/okr';
 import { OkrService } from '../services/okr.service';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home-detail',
@@ -13,12 +19,64 @@ import { switchMap } from 'rxjs/operators';
 export class HomeDetailComponent implements OnInit {
   okr$: Observable<Okr>;
 
-  constructor(private route: ActivatedRoute, public okrService: OkrService) {}
+  form = this.fb.group({
+    title: ['', [Validators.required, Validators.maxLength(40)]],
+    start: ['', [Validators.required, Validators.maxLength(40)]],
+    end: ['', [Validators.required, Validators.maxLength(40)]],
+    primaries: [],
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    public okrService: OkrService,
+    private fb: FormBuilder
+  ) {
+    this.route.paramMap
+      .pipe(
+        switchMap((map) => {
+          const id = map.get('id');
+          console.log(id);
+          return id ? this.okrService.getOkr(id) : of(null);
+        })
+      )
+      .subscribe((okr) => {
+        this.form.patchValue(okr);
+        console.log(okr);
+        okr.primaries.forEach((primary) =>
+          this.form.primaries.push({
+            primary: [primary, [Validators.required]],
+          })
+        );
+      });
+  }
+
+  get primaries(): FormArray {
+    return this.form.get('primaries') as FormArray;
+  }
+
+  // addPrimary(primary?: Primary) {
+  //   console.log(primary);
+  // const formGroup = this.fb.group({
+  //   primary: [primary?.primary || '', [Validators.required]],
+  // });
+  // console.log(formGroup);
+  //   this.form.primaries.push(primary);
+  // }
+
+  rename() {
+    this.okr$ = this.route.paramMap.pipe(
+      switchMap((map) => {
+        const id = map.get('id');
+        return this.okrService.getOkr(id);
+      })
+    );
+  }
 
   ngOnInit() {
     this.okr$ = this.route.paramMap.pipe(
       switchMap((map) => {
         const id = map.get('id');
+        console.log(id);
         return this.okrService.getOkr(id);
       })
     );
