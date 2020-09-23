1fd1ca4c994bb0d92b585f32b676f4d596a83c67
peyo-code <mr233425@gmail.com>
2020-09-23 17:05:15 +0900

    fix(home-detail): テーブル修正

--- src/app/home-detail/home-detail.component.ts
+++ src/app/home-detail/home-detail.component.ts
@@ -20,11 +20,13 @@ import {
 export class HomeDetailComponent implements OnInit {
   okr$: Observable<Okr>;
 
+  // tableのフォームを配列で持たせておく
+  tableForms = [];
+
   // formArray
   tableForm: FormArray = this.fb.array([]);
 
-  // formgroup
-  addRow() {
+  addRow(i) {
     const row = this.fb.group({
       title: ['', [Validators.required]],
       target: ['', [Validators.required]],
@@ -33,7 +35,6 @@ export class HomeDetailComponent implements OnInit {
       lastUpdated: ['', [Validators.required]],
     });
     this.tableForm.push(row);
-    console.log(row);
   }
 
   get rows(): FormGroup[] {
@@ -51,11 +52,36 @@ export class HomeDetailComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    // オブザーバブルを使い、URLからOkrsを流すmapはreturn
     this.okr$ = this.route.paramMap.pipe(
+      // 別のオブザーバブルに変える
       switchMap((map) => {
+        // mapでidを文字列として取得
         const id = map.get('id');
+        console.log(id);
         return this.okrService.getOkr(id);
       })
     );
+    // subscribeでokrsを取得
+    this.okr$.subscribe((okrs) => console.log(okrs));
+    // i番目のprimariesが入ってきた時のrowを作成
+    this.okr$.forEach((primaries) => {
+      const tableForm = this.fb.array([]);
+      this.tableForms.push(tableForm);
+      console.log(tableForm);
+    });
   }
+
+  // サブスクライブで取得したokrsの中のprimariesをループ
+  // okr.primaries.forEach((primary) => {
+  //   const tableForm = this.fb.array([]);
+  //   this.tableForms.push(tableForm);
+  // });
+
+  // i番目のテーブルのレコードを追加
+  // addRow(i: number) {
+  // formArray(行)に対するpush
+  //   this.tableForm[i].push(this.row);
+  //   console.log(this.row);
+  // }
 }
