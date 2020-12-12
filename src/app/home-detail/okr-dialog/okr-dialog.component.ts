c5a015f0e2c0559ad1da90bf35a8fbcb139794f1
peyo-code <mr233425@gmail.com>
2020-12-12 13:55:16 +0900

    feat(home):UI・component分割

--- src/app/home-detail/okr-dialog/okr-dialog.component.ts
+++ src/app/home-detail/okr-dialog/okr-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { OkrService } from 'src/app/services/okr.service';
 
 @Component({
   selector: 'app-okr-dialog',
@@ -8,12 +11,20 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class OkrDialogComponent implements OnInit {
   constructor(
+    private route: ActivatedRoute,
+    private okrSearvice: OkrService,
     @Inject(MAT_DIALOG_DATA)
     public data: {
-      tableTitle;
+      primaries;
       tableData;
     }
-  ) {}
+  ) {
+    this.route.queryParamMap.pipe(
+      switchMap((params) => {
+        return this.okrSearvice.getPrimaries(params.get('id'));
+      })
+    );
+  }
 
   ngOnInit(): void {}
 }
