9ed77c5214c5569390ad38f7084c40948de20d4b
peyo-code <mr233425@gmail.com>
2020-12-19 21:11:40 +0900

    feat(okr-detail):okr-detail.component生成

---
+++ src/app/home-detail/okr-dialog/okr-detail/okr-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-okr-detail',
+  templateUrl: './okr-detail.component.html',
+  styleUrls: ['./okr-detail.component.scss'],
+})
+export class OkrDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
