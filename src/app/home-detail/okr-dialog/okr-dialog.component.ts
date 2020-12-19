9c18f7a0e48903f59f9564a401275ef9d1f74ee0
peyo-code <mr233425@gmail.com>
2020-12-19 13:57:21 +0900

    style(okr-dialog):リストコード整形

--- src/app/home-detail/okr-dialog/okr-dialog.component.ts
+++ src/app/home-detail/okr-dialog/okr-dialog.component.ts
@@ -6,6 +6,25 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./okr-dialog.component.scss'],
 })
 export class OkrDialogComponent implements OnInit {
+  readonly lists = [
+    {
+      menu: 'target',
+      text: 'テスト',
+    },
+    {
+      menu: 'current',
+      text: 'テスト',
+    },
+    {
+      menu: 'percentage',
+      text: 'テスト',
+    },
+    {
+      menu: 'lastUpdated',
+      text: 'テスト',
+    },
+  ];
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
