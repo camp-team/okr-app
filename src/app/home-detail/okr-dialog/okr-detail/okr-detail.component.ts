e1e979a60e9e7c9caf37058aab66575df3f74072
peyo-code <mr233425@gmail.com>
2020-12-29 11:57:41 +0900

    fix(okr-detail):誤字修正

--- src/app/home-detail/okr-dialog/okr-detail/okr-detail.component.ts
+++ src/app/home-detail/okr-dialog/okr-detail/okr-detail.component.ts
@@ -9,7 +9,7 @@ import { OkrService } from 'src/app/services/okr.service';
   styleUrls: ['./okr-detail.component.scss'],
 })
 export class OkrDetailComponent implements OnInit {
-  subTasks$: Observable<SubTask>;
+  subTask$: Observable<SubTask>;
 
   readonly lists = [
     {
@@ -37,7 +37,7 @@ export class OkrDetailComponent implements OnInit {
       const okrId = params.get('okrId');
       const primaryId = params.get('primaryId');
       const subTaskId = params.get('subTaskId');
-      this.subTasks$ = this.okrService.getSubTask(okrId, primaryId, subTaskId);
+      this.subTask$ = this.okrService.getSubTask(okrId, primaryId, subTaskId);
     });
   }
 }
