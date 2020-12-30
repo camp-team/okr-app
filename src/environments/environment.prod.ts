c8bb94ded086f25a6a8b3be6d4d22f3b24b87a56
peyo-code <mr233425@gmail.com>
2020-12-30 13:20:48 +0900

    feat(environment.prod):firebase情報追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDn9iFnVkalY34LDtOZN8EIfESlYVDihtA',
+    authDomain: 'okr-app-de4fa.firebaseapp.com',
+    databaseURL: 'https://okr-app-de4fa.firebaseio.com',
+    projectId: 'okr-app-de4fa',
+    storageBucket: 'okr-app-de4fa.appspot.com',
+    messagingSenderId: '104962990037',
+    appId: '1:104962990037:web:a6c1523b55ee08e88ea75c',
+    measurementId: 'G-YHBESMER0K',
+  },
 };
