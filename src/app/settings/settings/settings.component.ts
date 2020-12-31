11279e9d282c0d74c79e3900c9d3d4557000d239
peyo-code <mr233425@gmail.com>
2020-12-31 16:47:56 +0900

    feat(settings):ファイル選択時にダイアログを表示する

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -6,7 +6,9 @@ import { Router } from '@angular/router';
 import { User } from 'firebase';
 import { Observable } from 'rxjs';
 import { DeleteAccountDialogComponent } from 'src/app/delete-account-dialog/delete-account-dialog.component';
+import { ImageCropDialogComponent } from 'src/app/image-crop-dialog/image-crop-dialog.component';
 import { AuthService } from 'src/app/services/auth.service';
+// import { ImageCropDialogComponent } from '../image-crop-dialog/image-crop-dialog.component';
 
 @Component({
   selector: 'app-settings',
@@ -23,6 +25,14 @@ export class SettingsComponent implements OnInit {
       width: '450px',
       autoFocus: false,
       restoreFocus: false,
+    });
+  }
+
+  openImageCropDialog(event: any, imageSelecter: any): void {
+    this.dialog.open(ImageCropDialogComponent, {
+      autoFocus: false,
+      restoreFocus: false,
+      data: { event, imageSelecter },
     });
   }
 }
