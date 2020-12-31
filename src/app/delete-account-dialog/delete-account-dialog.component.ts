d4a1fb37188625c79bff2473963adf3652257c5e
peyo-code <mr233425@gmail.com>
2020-12-31 14:28:15 +0900

    fix(delete-account-dialog):退会処理をダイアログ画面に移行

--- src/app/delete-account-dialog/delete-account-dialog.component.ts
+++ src/app/delete-account-dialog/delete-account-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-delete-account-dialog',
@@ -6,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-account-dialog.component.scss'],
 })
 export class DeleteAccountDialogComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private fns: AngularFireFunctions,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+  deleteAccount() {
+    const callable = this.fns.httpsCallable('deleteAfUser');
+    return callable(this.authService.uid)
+      .toPromise()
+      .then(() => {
+        this.router.navigateByUrl('/');
+        this.authService.afAuth.signOut().then(() => {
+          this.snackBar.open(
+            'アカウントを削除しました。反映には時間がかかります。'
+          );
+        });
+      });
+  }
 }
