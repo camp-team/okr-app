b43be51c4f7af4aacebcf4e86eee1f9f74731534
peyo-code <mr233425@gmail.com>
2021-01-17 14:40:08 +0900

    feat(home-detail-title):titleの初期値をセット

--- src/app/home-detail-title/home-detail-title.component.ts
+++ src/app/home-detail-title/home-detail-title.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Okr } from '../interfaces/okr';
@@ -11,9 +12,24 @@ import { OkrService } from '../services/okr.service';
 })
 export class HomeDetailTitleComponent implements OnInit {
   private okrId = this.route.snapshot.queryParamMap.get('v');
+  public formGroup: FormGroup;
   okr$: Observable<Okr> = this.okrService.getOkr(this.okrId);
 
-  constructor(private route: ActivatedRoute, public okrService: OkrService) {}
+  constructor(
+    private route: ActivatedRoute,
+    public okrService: OkrService,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.okr$.subscribe((okr) => {
+      this.initTitle(okr.title);
+    });
+  }
+
+  initTitle(title: string) {
+    this.formGroup = this.fb.group({
+      title: [title, [Validators.required]],
+    });
+  }
 }
