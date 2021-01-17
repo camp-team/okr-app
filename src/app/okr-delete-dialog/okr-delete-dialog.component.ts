c9e8af35d02be4310b4bb34a75c8fc6295d5f671
peyo-code <mr233425@gmail.com>
2021-01-17 19:58:40 +0900

    feat(okr-delete-dialog):okr-delete-dialog生成

---
+++ src/app/okr-delete-dialog/okr-delete-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-okr-delete-dialog',
+  templateUrl: './okr-delete-dialog.component.html',
+  styleUrls: ['./okr-delete-dialog.component.scss'],
+})
+export class OkrDeleteDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
