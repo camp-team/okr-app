e6410dca212c0b996ad3f6bf01dbda49dfa3113e
peyo-code <mr233425@gmail.com>
2021-02-10 22:24:03 +0900

    feat(second-okr):UI作成

--- src/app/second-okr/second-okr.component.ts
+++ src/app/second-okr/second-okr.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { SecondOkr } from '../interfaces/second-okr';
+import { SecondOkrKeyResult } from '../interfaces/second-okr-key-result';
+import { AuthService } from '../services/auth.service';
+import { OkrService } from '../services/okr.service';
 
 @Component({
   selector: 'app-second-okr',
@@ -6,7 +12,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-okr.component.scss'],
 })
 export class SecondOkrComponent implements OnInit {
-  constructor() {}
+  private row: FormGroup;
+  public rows: {
+    [keyName: string]: FormArray;
+  } = {};
 
-  ngOnInit(): void {}
+  secondOkr$: Observable<SecondOkr[]> = this.okrService.getSecondOkrs();
+
+  constructor(
+    public okrService: OkrService,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.secondOkr$.subscribe((secondOkr) => {
+      secondOkr.forEach((secondOkr) => {
+        secondOkr.id;
+      });
+    });
+  }
+
+  okrId() {
+    this.secondOkr$.subscribe((secondOkr) => {
+      secondOkr.forEach((secondOkr) => {
+        secondOkr.id;
+      });
+    });
+  }
+
+  addRow(secondOkrId: string) {
+    this.row = this.fb.group({
+      key: ['', [Validators.required]],
+      target: ['', [Validators.required]],
+      current: ['', [Validators.required]],
+      percentage: ['', [Validators.required]],
+      lastUpdated: ['', [Validators.required]],
+    });
+    // this.rows[secondOkrId].push(this.row);
+    const formData = this.row.value;
+    const subTaskValue: Omit<SecondOkrKeyResult, 'id' | 'lastUpdated'> = {
+      secondOkrId,
+      key: formData.key,
+      target: formData.target,
+      current: formData.current,
+      percentage: formData.percentage,
+    };
+    this.okrService.createSecondOkrKeyResult(subTaskValue, secondOkrId);
+  }
 }
