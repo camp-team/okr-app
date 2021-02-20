d078547dca76a5ffe02e10fa9e2c750cb2793dee
peyo-code <mr233425@gmail.com>
2021-02-20 11:43:25 +0900

    feat(manage-footer):manage-footer.component作成

---
+++ src/app/manage-footer/manage-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-footer',
+  templateUrl: './manage-footer.component.html',
+  styleUrls: ['./manage-footer.component.scss'],
+})
+export class ManageFooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
