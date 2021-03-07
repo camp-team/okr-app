08b8e60f050272295aa5e19f015ef256e9175e64
peyo-code <mr233425@gmail.com>
2021-03-07 17:32:56 +0900

    feat(customer):顧客詳細の取得と請求管理URLの取得

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,24 +1,69 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Customer } from 'functions/src/interfaces/customer';
 import { Observable, of } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { shareReplay, switchMap } from 'rxjs/operators';
+import { shareReplay, switchMap, tap } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
+import Stripe from 'stripe';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
+  customerPortalURL: string;
   customer$: Observable<Customer> = this.afAuth.user.pipe(
     switchMap((user) => {
       if (user) {
-        return this.db.doc<Customer>(`customer/${user.uid}`).valueChanges();
+        return this.db.doc<Customer>(`customers/${user.uid}`).valueChanges();
       } else {
         return of(null);
       }
     }),
     shareReplay(1)
   );
+  stripeCustomer: Stripe.Customer;
+  // 顧客詳細の取得
+  private stripeCustomerSource$: Observable<
+    Stripe.Customer
+  > = this.customer$.pipe(
+    switchMap((customer) => {
+      if (customer) {
+        const callable = this.fns.httpsCallable('getStripeCustomer');
+        return callable(null);
+      } else {
+        return (this.customerPortalURL = null);
+      }
+    }),
+    // customerPortalURLを取得
+    tap((customer: Stripe.Customer) => {
+      if (customer) {
+        this.getStripeCustomerPortalURL();
+      } else {
+        this.customerPortalURL = null;
+      }
+    })
+  );
 
-  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private fns: AngularFireFunctions,
+    private ngZone: NgZone
+  ) {
+    this.stripeCustomerSource$.subscribe((customer) => {
+      this.stripeCustomer = customer;
+    });
+  }
+
+  async getStripeCustomerPortalURL() {
+    const callable = this.fns.httpsCallable('getStripeCustomerPortalURL');
+    this.ngZone.run(async () => {
+      this.customerPortalURL = await callable({})
+        .toPromise()
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }
 }
