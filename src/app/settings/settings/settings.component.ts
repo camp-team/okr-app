e64ed597c96e0be71c234ebe9305d1714f15e3d2
peyo-code <mr233425@gmail.com>
2021-03-07 17:34:48 +0900

    feat(settings):顧客詳細の取得

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -3,13 +3,18 @@ import { MatDialog } from '@angular/material/dialog';
 import { DeleteAccountDialogComponent } from 'src/app/delete-account-dialog/delete-account-dialog.component';
 import { ImageCropDialogComponent } from 'src/app/image-crop-dialog/image-crop-dialog.component';
 import { AuthService } from 'src/app/services/auth.service';
+import { CustomerService } from 'src/app/services/customer.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-  constructor(private dialog: MatDialog, public authService: AuthService) {}
+  constructor(
+    private dialog: MatDialog,
+    public authService: AuthService,
+    public customerService: CustomerService
+  ) {}
 
   ngOnInit(): void {}
 
