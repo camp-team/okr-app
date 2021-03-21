876af80735a64b132cad1984da983fbd3d174cd8
peyo-code <mr233425@gmail.com>
2021-03-21 12:52:56 +0900

    feat(dialog): ログイン時にダイアログを表示する

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,11 +9,16 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./cv.component.scss'],
 })
 export class CvComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   login() {
-    this.authService.login();
+    this.authService.login().then(() => {
+      this.dialog.open(LoginDialogComponent, {
+        autoFocus: false,
+        restoreFocus: false,
+      });
+    });
   }
 }
