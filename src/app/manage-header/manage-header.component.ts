6cde5fb51bd06920e6ee6f0cf9bb75f1cb056783
peyo-code <mr233425@gmail.com>
2021-03-28 13:49:40 +0900

    fix(manage-header): headerUI修正

--- src/app/manage-header/manage-header.component.ts
+++ src/app/manage-header/manage-header.component.ts
@@ -14,6 +14,9 @@ import { OkrService } from '../services/okr.service';
   styleUrls: ['./manage-header.component.scss'],
 })
 export class ManageHeaderComponent implements OnInit {
+  home = this.route.snapshot.params;
+  secondOkrPath = this.route.snapshot.params;
+  achieve = this.route.snapshot.params;
   user$ = this.authService.user$;
   isSecondOkr: boolean;
   avatarURL: string;
@@ -29,7 +32,8 @@ export class ManageHeaderComponent implements OnInit {
     public authService: AuthService,
     public okrService: OkrService,
     public loadingService: LoadingService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.loadingService.loading = true;
     this.authService.getUser(this.authService.uid).subscribe((result) => {
@@ -38,6 +42,7 @@ export class ManageHeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.home);
     this.secondOkrs$.subscribe((secondOkrs) => {
       if (secondOkrs.length === 0) {
         this.isSecondOkr = false;
