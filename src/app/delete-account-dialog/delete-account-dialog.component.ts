9b5e5674ff4c26c89ea93254e6b637bb76d94145
peyo-code <mr233425@gmail.com>
2021-04-16 01:54:21 +0900

    feat(delete-account-dialog):okrIdを持たせてドキュメント削除時に使用

--- src/app/delete-account-dialog/delete-account-dialog.component.ts
+++ src/app/delete-account-dialog/delete-account-dialog.component.ts
@@ -3,8 +3,11 @@ import { AngularFireFunctions } from '@angular/fire/functions';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Okr } from '../interfaces/okr';
 import { AuthService } from '../services/auth.service';
 import { LoadingService } from '../services/loading.service';
+import { OkrService } from '../services/okr.service';
 
 @Component({
   selector: 'app-delete-account-dialog',
@@ -12,18 +15,29 @@ import { LoadingService } from '../services/loading.service';
   styleUrls: ['./delete-account-dialog.component.scss'],
 })
 export class DeleteAccountDialogComponent implements OnInit {
+  Okrs$: Observable<Okr[]> = this.okrService.getOkrs();
+  okrId: string;
+
   constructor(
     private dialogRef: MatDialogRef<DeleteAccountDialogComponent>,
     private fns: AngularFireFunctions,
     private authService: AuthService,
     private router: Router,
     private snackBar: MatSnackBar,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private okrService: OkrService
   ) {}
 
-  ngOnInit(): void {}
-  deleteAccount() {
+  ngOnInit(): void {
+    this.Okrs$.subscribe((okrs) => {
+      okrs.forEach((okr) => {
+        this.okrId = okr.id;
+      });
+    });
+  }
+  deleteAccount(okrId: string) {
     this.loadingService.loading = true;
+    this.okrService.deleteOkr(okrId);
     const callable = this.fns.httpsCallable('deleteAfUser');
     this.dialogRef.close();
     return callable(this.authService.uid)
