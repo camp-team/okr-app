df05ec7fed46db656de150144a9b8fce113c4864
peyo-code <mr233425@gmail.com>
2021-04-24 13:51:37 +0900

    feat(okr-achievement):mat-button追加

--- src/app/okr-achievement/okr-achievement.module.ts
+++ src/app/okr-achievement/okr-achievement.module.ts
@@ -4,8 +4,15 @@ import { CommonModule } from '@angular/common';
 import { OkrAchievementRoutingModule } from './okr-achievement-routing.module';
 import { OkrAchievementComponent } from './okr-achievement/okr-achievement.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [OkrAchievementComponent],
-  imports: [CommonModule, OkrAchievementRoutingModule, MatIconModule],
+  imports: [
+    CommonModule,
+    OkrAchievementRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class OkrAchievementModule {}
