3f276d7b698c5218043b8253cc7a8b97d936605b
peyo-code <mr233425@gmail.com>
2021-05-01 02:00:15 +0900

    fix(home-routing):不要コード削除

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HomeDetailComponent } from '../home-detail/home-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  },
-  {
-    path: 'homedetail',
-    component: HomeDetailComponent,
   },
 ];
 
