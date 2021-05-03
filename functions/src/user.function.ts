903a46c459ca1a5aeeeb9af008e2147e6159fcc5
peyo-code <mr233425@gmail.com>
2021-05-03 19:34:54 +0900

    feat(user.function):okrコレクション追加

--- functions/src/user.function.ts
+++ functions/src/user.function.ts
@@ -34,6 +34,9 @@ export const deleteCollection = functions
   .region('asia-northeast1')
   .auth.user()
   .onDelete((user) => {
+    const okrsRef = db
+      .collectionGroup(`okrs`)
+      .where('creatorId', '==', user.uid);
     const primariesRef = db
       .collectionGroup(`primaries`)
       .where('uid', '==', user.uid);
@@ -50,6 +53,7 @@ export const deleteCollection = functions
       .collection(`customers`)
       .where('userId', '==', user.uid);
     return Promise.all([
+      deleteCollectionByReference(okrsRef),
       deleteCollectionByReference(primariesRef),
       deleteCollectionByReference(secondOkrKeyResultsRef),
       deleteCollectionByReference(secondOkrObjectsRef),
