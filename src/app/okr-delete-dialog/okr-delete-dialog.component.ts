2aa92673cab66439be2eca9ff193fc674d71c80f
peyo-code <mr233425@gmail.com>
2021-05-05 01:22:07 +0900

    feat(okr-delete):削除時にローディングUI表示

--- src/app/okr-delete-dialog/okr-delete-dialog.component.ts
+++ src/app/okr-delete-dialog/okr-delete-dialog.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { LoadingService } from '../services/loading.service';
 import { OkrService } from '../services/okr.service';
 
 @Component({
@@ -14,15 +16,27 @@ export class OkrDeleteDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA)
     public data: { okrId: string },
     private okrService: OkrService,
-    private dialogRef: MatDialogRef<OkrDeleteDialogComponent>
+    private dialogRef: MatDialogRef<OkrDeleteDialogComponent>,
+    private fns: AngularFireFunctions,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit(): void {}
 
   okrDelete() {
-    this.okrService.deleteOkr(this.data.okrId);
-    this.okrService.deleteOkrDocument(this.data.okrId);
+    this.loadingService.loading = true;
+    const callable = this.fns.httpsCallable('deleteOkr');
     this.dialogRef.close();
+    callable(this.data.okrId)
+      .toPromise()
+      .then(() => {
+        this.okrService.deleteOkrDocument(this.data.okrId);
+        this.loadingService.loading = false;
+        this.router.navigateByUrl('/manage/home');
+        this.snackBar.open('削除しました', '');
+      });
   }
 
   closeDeleteOKRDialog() {
