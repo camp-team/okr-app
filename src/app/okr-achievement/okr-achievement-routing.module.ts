30f8c49e1b930cb94fe978770221526999cb438d
peyo-code <mr233425@gmail.com>
2021-05-07 00:36:14 +0900

    fix(okr-achievement):不要コード削除

--- src/app/okr-achievement/okr-achievement-routing.module.ts
+++ src/app/okr-achievement/okr-achievement-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CompletedChildOkrComponent } from '../completed-child-okr/completed-child-okr.component';
 import { OkrAchievementComponent } from './okr-achievement/okr-achievement.component';
 
 const routes: Routes = [
   {
     path: '',
     component: OkrAchievementComponent,
-    children: [
-      {
-        path: ':secondOkrId',
-        component: CompletedChildOkrComponent,
-      },
-    ],
   },
 ];
 
