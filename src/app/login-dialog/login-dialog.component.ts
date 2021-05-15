3ae42daf281f6d2508eb5dde2c0ef3651c7b8c01
peyo-code <mr233425@gmail.com>
2021-05-16 02:32:52 +0900

    feat(login-dialog):ユーザー名取得

--- src/app/login-dialog/login-dialog.component.ts
+++ src/app/login-dialog/login-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login-dialog',
@@ -8,12 +11,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-dialog.component.scss'],
 })
 export class LoginDialogComponent implements OnInit {
+  user$: Observable<User> = this.authService.user$;
+  userName: string;
+
   constructor(
     private dialogRef: MatDialogRef<LoginDialogComponent>,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user$.subscribe((user) => {
+      this.userName = user.name;
+    });
+  }
 
   close() {
     this.dialogRef.close();
