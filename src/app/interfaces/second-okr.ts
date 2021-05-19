4123e1827cb3619ccbb191ea50d5894396b0ef47
peyo-code <mr233425@gmail.com>
2021-05-20 07:48:29 +0900

    fix(second-okr):命名修正

--- src/app/interfaces/second-okr.ts
+++
@@ -1,9 +0,0 @@
-import * as firebase from 'firebase';
-
-export interface SecondOkr {
-  end?: firebase.default.firestore.Timestamp;
-  secondOkrObjects?: string;
-  creatorId?: string;
-  secondOkrId?: string;
-  isComplete: boolean;
-}
