4d39e47d1615e14d9785e31355bef3139c7d59a4
peyo-code <mr233425@gmail.com>
2021-05-20 07:48:40 +0900

    fix(mange-footer):命名修正

--- src/app/manage-footer/manage-footer.component.ts
+++ src/app/manage-footer/manage-footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { SecondOkr } from '../interfaces/second-okr';
+import { ChildOkr } from '../interfaces/child-okr';
 import { AuthService } from '../services/auth.service';
 import { LoadingService } from '../services/loading.service';
 import { OkrService } from '../services/okr.service';
@@ -15,9 +15,9 @@ import { OkrService } from '../services/okr.service';
 export class ManageFooterComponent implements OnInit {
   isSecondOkr: boolean;
   isComplete: boolean;
-  secondOkr: SecondOkr;
-  secondOkrs$: Observable<SecondOkr[]> = this.okrService
-    .getSecondOkrs()
+  secondOkr: ChildOkr;
+  secondOkrs$: Observable<ChildOkr[]> = this.okrService
+    .getChildOkrs()
     .pipe(tap(() => (this.loadingService.loading = false)));
 
   constructor(
@@ -37,15 +37,15 @@ export class ManageFooterComponent implements OnInit {
         this.isSecondOkr = true;
       }
       secondOkrs.map((secondOkr) => {
-        this.isComplete = secondOkr.isComplete;
-        if (secondOkr.isComplete === true) this.secondOkr = secondOkr;
+        this.isComplete = secondOkr.isChildOkrComplete;
+        if (secondOkr.isChildOkrComplete === true) this.secondOkr = secondOkr;
       });
     });
   }
 
   progress() {
     this.router.navigate(['manage/secondOkr'], {
-      queryParams: { v: this.secondOkr.secondOkrId },
+      queryParams: { v: this.secondOkr.isChildOkrComplete },
     });
   }
 }
