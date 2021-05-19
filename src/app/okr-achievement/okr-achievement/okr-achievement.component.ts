3dcaeae8ad7486c3e5c4d72a8385234db66e29a2
peyo-code <mr233425@gmail.com>
2021-05-20 07:49:10 +0900

    fix(okr-achievement):命名修正

--- src/app/okr-achievement/okr-achievement/okr-achievement.component.ts
+++ src/app/okr-achievement/okr-achievement/okr-achievement.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteSecondOkrDialogComponent } from 'src/app/delete-second-okr-dialog/delete-second-okr-dialog.component';
+import { ChildOkr } from 'src/app/interfaces/child-okr';
 import { OkrService } from 'src/app/services/okr.service';
 
 @Component({
@@ -16,25 +17,25 @@ export class OkrAchievementComponent implements OnInit {
 
   ngOnInit(): void {
     this.okrService.childOkrs$.subscribe((childOkrs) => {
-      childOkrs.map((childOkr) => {
+      childOkrs.map((childOkr: ChildOkr) => {
         if (childOkrs.length === 0) {
           this.childOkr = false;
-        } else if (childOkr.isComplete === true) {
+        } else if (childOkr.isChildOkrComplete === true) {
           this.childOkr = false;
-          this.childOkrId = childOkr.secondOkrId;
-        } else if (childOkr.isComplete === false) {
+          this.childOkrId = childOkr.childOkrId;
+        } else if (childOkr.isChildOkrComplete === false) {
           this.childOkr = true;
         }
       });
     });
   }
 
-  deleteSecondOkr(secondOkrId) {
+  deleteSecondOkr(secondOkrId: ChildOkr) {
     this.dialog.open(DeleteSecondOkrDialogComponent, {
       autoFocus: false,
       restoreFocus: false,
       data: {
-        secondOkrId: secondOkrId,
+        secondOkrId,
       },
     });
   }
