53711f9f0447111cf736052a240bb4ddb058765a
peyo-code <mr233425@gmail.com>
2021-05-23 21:29:17 +0900

    fix(home):不要コード削除

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-import { ParentOkrComponent } from './parent-okr/parent-okr.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
