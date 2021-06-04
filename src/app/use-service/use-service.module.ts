4a46141f1da005d393c1237ae2d1403086fc6ab3
peyo-code <mr233425@gmail.com>
2021-06-04 22:43:07 +0900

    feat(use-service):shared-moduleと紐付け

--- src/app/use-service/use-service.module.ts
+++ src/app/use-service/use-service.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UseServiceRoutingModule } from './use-service-routing.module';
 import { UseServiceComponent } from './use-service/use-service.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UseServiceComponent],
-  imports: [CommonModule, UseServiceRoutingModule],
+  imports: [CommonModule, UseServiceRoutingModule, SharedModule],
 })
 export class UseServiceModule {}
