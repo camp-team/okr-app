fea969dc34ae63b252b5b88ef1b4e27f31533b0a
peyo-code <mr233425@gmail.com>
2021-06-06 13:32:46 +0900

    fix(legal):UI修正

--- src/app/legal/legal.module.ts
+++ src/app/legal/legal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LegalRoutingModule } from './legal-routing.module';
 import { LegalComponent } from './legal/legal.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LegalComponent],
-  imports: [CommonModule, LegalRoutingModule],
+  imports: [CommonModule, LegalRoutingModule, SharedModule],
 })
 export class LegalModule {}
